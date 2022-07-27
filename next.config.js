3b10d06bc7a070e49adff79715075928d03515be
waitingwittykitty <hryschiller@gmail.com>
2022-07-27 10:05:18 -0700

    chore: added svgr to webpack and forms to tailwind

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
-};
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+  
+    return config;
+  },
+});
 
 module.exports = nextConfig;
