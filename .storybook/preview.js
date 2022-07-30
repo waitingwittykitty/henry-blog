d582ec8e83e670868323c2ff286edd4c97df4bcb
waitingwittykitty <hryschiller@gmail.com>
2022-07-29 20:37:29 -0700

    refactor: prettified .storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,7 @@ import { MockedProvider } from '@apollo/client/testing';
 import '../styles/globals.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -12,5 +12,5 @@ export const parameters = {
   },
   apolloClient: {
     MockedProvider,
-  }
-}
+  },
+};
