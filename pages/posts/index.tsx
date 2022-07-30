0deeac2bead44a0fd8ad4dd75a5f9cf7d45667d6
waitingwittykitty <hryschiller@gmail.com>
2022-07-30 01:25:45 -0700

    feat: added redirect after login in post list page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { useFeedQuery } from '@/api';
+import { useCurrentUserDetailsQuery, useFeedQuery } from '@/api';
 import Sidebar from '@/components/sidebar';
 import NoData from '@/components/no-data';
 import PostList from '@/components/post-list';
@@ -9,19 +9,28 @@ import { Spinner } from '@/components';
 
 function Posts() {
   const { data, loading } = useFeedQuery();
+  const { data: currentUserResponse } = useCurrentUserDetailsQuery({});
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  const isAnonymousUser = !currentUserResponse?.me!;
 
   const hasNoData = !data || data?.feed?.length <= 0;
 
   return (
     <section className="flex">
-      <Sidebar />
+      <Sidebar className="shrink-0" />
 
       <section className="p-6 flex-grow">
-        <div className="flex">
+        <div className="flex mb-5">
           <h1 className="flex-auto text-2xl">All Posts</h1>
 
-          <Link href="/posts/create">
-            <a className="btn bg-indigo-500 hover:bg-indigo-600 text-white p-2 rounded">
+          <Link
+            href={isAnonymousUser ? '/account/login?next=/posts/create' : '/posts/create'}
+          >
+            <a className="btn bg-sky-500 hover:bg-sky-600 text-white p-2 rounded-lg">
               Create Post
             </a>
           </Link>
