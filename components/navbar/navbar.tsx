82ad7ba0e6841ce28eff7ed4e5e92580ca86f93f
waitingwittykitty <hryschiller@gmail.com>
2022-07-29 20:32:55 -0700

    refactor: long classnames in navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -21,13 +21,19 @@ export function Navbar() {
   return (
     <div
       className={clsx(
-        'sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500',
-        'lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-slate-50/75',
-        'backdrop-blur dark:bg-slate-900/75'
+        'sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors',
+        'duration-500 lg:z-50 lg:border-b lg:border-slate-900/10',
+        'dark:border-slate-50/[0.06] bg-slate-50/75 backdrop-blur',
+        'dark:bg-slate-900/75'
       )}
     >
       <div className="max-w-8xl mx-auto">
-        <div className="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
+        <div
+          className={clsx(
+            'py-4 border-b border-slate-900/10 lg:px-8 lg:border-0',
+            'dark:border-slate-300/10 mx-4 lg:mx-0'
+          )}
+        >
           <div className="relative flex items-center">
             <Link href="/" passHref>
               <a
