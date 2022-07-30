df1f86bfa9f9ef2a7dbf9ca382764ef8fca933ac
waitingwittykitty <hryschiller@gmail.com>
2022-07-30 01:20:11 -0700

    fix: bug in navigation-panel

--- components/navigation-panel/navigation-panel.tsx
+++ components/navigation-panel/navigation-panel.tsx
@@ -1,17 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
 
-import { usePaths } from '@/libs/paths';
-
 export function NavigationPanel() {
-  const paths = usePaths();
-
   const linkClassname =
     'flex p-4 items-center w-full rounded-md shadow-sm h-10 hover:text-blue-500';
 
   return (
     <div className="group w-full md:w-4/5 cursor-default rounded-md bg-white">
-      <Link href={paths.account.preferences.$url()} passHref>
+      <Link href="/account/preferences" passHref>
         <a href="pass" className="text-black">
           <span className={linkClassname}>Account preferences</span>
         </a>
