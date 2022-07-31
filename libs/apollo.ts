8afdb2bfd1138f3175637395d7a749147bc30222
waitingwittykitty <hryschiller@gmail.com>
2022-07-31 02:50:35 -0700

    fix: fixed bug with local-storage in apollo-client

--- libs/apollo.ts
+++ libs/apollo.ts
@@ -7,7 +7,11 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('henry-blog-token');
+  let token = null;
+
+  if (global.localStorage) {
+    token = localStorage.getItem('henry-blog-token');
+  }
 
   return {
     headers: {
