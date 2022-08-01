fe842f8ea737e224346852040fa7602a3bcfaf3b
waitingwittykitty <hryschiller@gmail.com>
2022-07-31 18:06:12 -0700

    fix: fixed validation message in create-post

--- pages/posts/create.tsx
+++ pages/posts/create.tsx
@@ -33,7 +33,7 @@ function PostCreate() {
     <main>
       <h1 className="text-2xl p-5">Create Post</h1>
 
-      <form action="/posts/create" onSubmit={handleSubmit}>
+      <form method="post" action="/posts" onSubmit={handleSubmit}>
         <div className="mb-4">
           <div className="flex items-center">
             <label className="mr-2" htmlFor="title">
@@ -44,7 +44,7 @@ function PostCreate() {
               className="flex-auto px-4 border-2 py-2 rounded-md text-sm outline-none"
               type="title"
               id="title"
-              {...register('title', { required: 'The field Title is required' })}
+              {...register('title', { required: 'Title is required' })}
             />
           </div>
 
